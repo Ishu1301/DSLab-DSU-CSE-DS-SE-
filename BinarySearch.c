4db940f41dce630d80f2361b300667b5856ce0cb
Ishaan Shukla <78337991+Ishu1301@users.noreply.github.com>
2026-09-05 15:57:11 +0530

    Bug fix
    program was unable to find the element at the first and last index

--- BinarySearch.c
+++ BinarySearch.c
@@ -32,19 +32,21 @@ int main(){
 	}
 	
 }
-int binarySearch(int*arr,int arrSize,int target){
-	int l=0,r=arrSize-1;
-	while(l<r){
-		int mid = (l+r)/2;
-		if(target==arr[mid]){
-			return mid;
-		}
-		else if(target<arr[mid]){
-			l = mid+1;
-		}
-		else{
-			r = mid-1;
-		}
-	}
-	return -1;
-}
+int binarySearch(int* arr, int arrsize, int target) {
+    int l = 0;
+    int r = arrsize - 1;
+    while (l <= r) {
+        int mid = l + (r - l) / 2;
+        
+        if (arr[mid] == target) {
+            return mid;
+        }
+        else if (target > arr[mid]) {
+            l = mid + 1;
+        }
+        else {
+            r = mid - 1;
+        }
+    }
+    return -1;
+}   
